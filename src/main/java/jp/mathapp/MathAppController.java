ee1a57b645f2c1740977c241cdc42046b2aefd87
ota <otausuk@gmail.com>
2017-01-09 16:40:22 +0900

    First commit

---
+++ src/main/java/jp/mathapp/MathAppController.java
@@ -0,0 +1,22 @@
+package jp.mathapp;
+
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class MathAppController {
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String get(Locale locale, Model model) {
+		return "top";
+	}
+
+
+
+}
